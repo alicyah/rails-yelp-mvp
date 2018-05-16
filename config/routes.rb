5451f7c26660205388f26dbbd4a14deb88958408
Alicia Yahiaoui <alicia.yahiaoui@gmail.com>
2018-05-16 18:42:07 +0200

    Yelp MVC done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "restaurants#index"
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: :new_review
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: "The #{Faker::Color.color_name.capitalize}",
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number,
+    )
+  restaurant.save!
+end
+puts "Finished"
+
